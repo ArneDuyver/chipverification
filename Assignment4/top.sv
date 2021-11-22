5549f2fa92fc9bbd7c504d27c4a74204e2bf0c9d
ArneDuyver <arne_duyver@hotmail.com>
2021-11-22 20:00:50 +0100

    Ass4: changed

--- Assignment4/top.sv
+++ Assignment4/top.sv
@@ -34,7 +34,7 @@ module top;
     option.at_least = 100;
     cover_point: coverpoint ifc.instruction
     iff(ifc.valid) {
-      bins bin_only_a0 = { 8'ha0 };
+      bins bin_only_a0 = { 160 };
     }
   endgroup
 
